a1004165f590ba44face76637d9c5e72c0aa05ed
tuakenglue0704-blip <tuakeng.lue0704@gmail.com>
2026-07-06 12:21:30 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1. นําเข้า Express.js (เป็น Framework เครื่องมือทุ่นแรงที่ช่วยให้เราสร้าง Web Server ได้ง่ายและสั้นลง)
+const express = require("express");
+const app = express();
+// 2. สร้างเส้นทาง (Route) สําหรับหน้าแรก
+// เมื่อมีผู้ใช้พิมพ์ URL เข้ามาที่หน้าแรก ("/") Server จะรับคําร้องขอ (request) และตอบกลับ
+(response)
+app.get("/", (request, response) => {
+// 3. สิ่งที่ Server จะส่งกลับไปแสดงผลบนหน้าจอผู้ใช้ (ใช้ภาษา HTML ในการจัดรูปแบบ)
+response.send(`
+<div style="font-family: sans-serif; text-align: center; margin-top: 50px;">
+<h1>ยินดีต้อนรับสู่ Server ของผม/หนู</h1>
+<p>นี่คือ Web Server เครื่องแรกที่สร้างขึ้นเองด้วย Node.js ครับ/ค่ะ</p>
+<hr>
+<h2>ข้อมูลนักศึกษา</h2>
+<p><strong>รหัสนักศึกษา:</strong> [69319010131]</p>
+<p><strong>ชื่อ-นามสกุล:</strong> [นาย ณัชพล ลือกระจ่าง]</p>
+</div>
+`);
+});
